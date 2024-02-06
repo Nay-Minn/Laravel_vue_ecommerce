96a207d42505446a936f8fbc5a5fbe6a19f77994
Nay-Minn <mr.nayminnhtet@gmail.com>
2024-02-06 17:54:29 +0630

    create project and install vue,tailwind,vuex and test store

---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        test: "1234ABCD",
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+});
+
+export default store;
